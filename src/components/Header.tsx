2bad5e3ba75dd1fe9b325b73a4fe05a13ddfc1d4
Lautaro Moreno <lemoreno2002@gmail.com>
2025-07-07 20:32:42 -0300

    Normalización del largo del navBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,3 @@
-// import { Link } from "react-router-dom";
-// import "./Header.css";
-
-// export default function Header() {
-//   return (
-//     <header className="header">
-//       <div className="logo">
-//         <span className="l">L</span>
-//         <span className="e">E</span>
-//         <span className="m">M</span>
-//       </div>
-//       <nav className="nav">
-//         <Link to="/">&lt; Inicio /&gt;</Link>
-//         <Link to="/proyectos">&lt; Proyectos /&gt;</Link>
-//         <Link to="/skills">&lt; Skills /&gt;</Link>
-//         <Link to="/sobre-mi">&lt; Sobre mí /&gt;</Link>
-//         <Link to="/contacto">&lt; Contacto /&gt;</Link>
-//       </nav>
-//     </header>
-//   );
-// }
-
 import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import { Menu, X } from 'lucide-react'; // Iconos para el menú móvil
